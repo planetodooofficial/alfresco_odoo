1dd04da526e510ea74307c748bf8090258f642ca
root <you@example.com>
2020-01-17 18:27:31 +0530

    alfresco operations (Authentication API and Discovery API)

---
+++ alfresco_odoo_connector/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Alfresco API',
+    'version': '1.0',
+    'summary': 'Alfresco Document Management System integration with Odoo',
+    'category': 'API',
+    'author': 'Planet-Odoo',
+    'maintainer': 'Planet-Odoo',
+    'company': 'Planet-Odoo',
+    'website': '',
+    'depends': ['base'],
+    'data': [
+        'view/alfresco_operation_view.xml'
+    ],
+    'images': [],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
