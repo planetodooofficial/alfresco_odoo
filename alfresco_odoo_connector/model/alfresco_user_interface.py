2524d596b8c7bd26b515c203f2e340eb0b353a4e
root <you@example.com>
2020-03-18 18:01:21 +0530

    new module added.

--- alfresco_odoo_connector/model/alfresco_user_interface.py
+++ alfresco_odoo_connector/model/alfresco_user_interface.py
@@ -14,7 +14,7 @@ class SaleOrderInherit(models.Model):
     is_active = fields.Boolean('Folder Created', default=False)
     relative_path = fields.Char('Path', default='/Odoo/Sales Order/')
     attachment_count = fields.Integer('Count')
-    sale_order_id = fields.Char('Sale Order ID')
+    order_id = fields.Char('Sale Order ID')
 
     @api.model
     def default_get(self, default_field):
@@ -159,21 +159,6 @@ class SaleOrderInherit(models.Model):
                 'views': False,
                 'type': 'ir.actions.act_window',
             }
-
-        # elif response.status_code == 409:
-        #     data_response_2 = json.loads(response_2.text)
-        #     wiz_ob = self.env['pop.folder'].create({'pop_up': data_response_2["error"]["errorKey"]})
-        #     return {
-        #         'name': _('Create Folder'),
-        #         'view_type': 'form',
-        #         'view_mode': 'form',
-        #         'res_model': 'pop.folder',
-        #         'res_id': wiz_ob.id,
-        #         'view_id': False,
-        #         'target': 'new',
-        #         'views': False,
-        #         'type': 'ir.actions.act_window',
-        #     }
         else:
             wiz_ob = self.env['pop.folder'].create({'pop_up': "Please check your request and try again!"})
             return {
@@ -188,10 +173,105 @@ class SaleOrderInherit(models.Model):
                 'type': 'ir.actions.act_window',
             }
 
-    def upload_file(self):
-        """Uploading a file to the Repository means creating a node with metadata and content."""
 
-        response = False
+class PurchaseOrderInherit(models.Model):
+    _inherit = 'purchase.order'
+
+    notebook_ids = fields.One2many('alf.ui.functionality', 'purchase_id', string="Documents List")
+    # search_folder = fields.Many2one('folder.details', string="Folder")
+    is_active = fields.Boolean('Folder Created', default=False)
+    relative_path = fields.Char('Path', default='/Odoo/Purchase Order/')
+    attachment_count = fields.Integer('Count')
+    order_id = fields.Char('Purchase Order ID')
+
+    @api.model
+    def default_get(self, default_field):
+        res = super(PurchaseOrderInherit, self).default_get(default_field)
+        if self._context.get('path'):
+            res['alf_relative_path'] = self._context.get('path')
+        return res
+
+    @api.model
+    def create(self, vals):
+        res = super(PurchaseOrderInherit, self).create(vals)
+        res.update({'order_id': res.name})
+        return res
+
+    def display_count_attachment(self):
+        pass
+
+    def save_document_content(self):
+
+        ticket = self.env['alfresco.operations'].search([], limit=1)
+        ticket.get_auth_token_header()
+
+        folder = self.env['folder.details'].search([('name', '=', self.order_id)])
+
+        get_file_url = str(ticket.alf_base_url) + 'alfresco/api/-default-/public/alfresco/versions/1/nodes/' + str(
+            folder.folder_id) + '/children'
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic' " " + str(ticket.alf_encoded_ticket)
+        }
+
+        response = requests.get(get_file_url, headers=headers)
+        if response.status_code == 200:
+            response_data = json.loads(response.text)
+            self.attachment_count = response_data['list']['pagination']['count']
+
+            result = self._cr.execute('delete from alf_ui_functionality')
+
+            if response_data['list']['pagination']['count'] >= 1:
+                for record in response_data['list']['entries']:
+                    if record['entry']['name']:
+                        name = record['entry']['name']
+                        doc_id = record['entry']['id']
+                        document_vals = [(0, 0, {
+                            'order_id': self.id,
+                            'document_name': name,
+                            'document_id': doc_id
+                        })]
+                        existing = self.notebook_ids.search([('document_id', '=', doc_id),
+                                                             ('document_name', '=', name)])
+                        if existing:
+                            existing.write({
+                                'document_name': name,
+                                'document_id': doc_id
+                            })
+                        else:
+                            self.update({'notebook_ids': document_vals})
+                            self._cr.commit()
+                wiz_ob = self.env['pop.auth'].create(
+                    {'pop_up': 'Files Exported Successfully.'})
+                return {
+                    'name': _('Refresh Repository'),
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'res_model': 'pop.auth',
+                    'res_id': wiz_ob.id,
+                    'view_id': False,
+                    'target': 'new',
+                    'views': False,
+                    'type': 'ir.actions.act_window',
+                }
+            else:
+                wiz_ob = self.env['pop.auth'].create(
+                    {'pop_up': 'Folder does not contains any files.'})
+                return {
+                    'name': _('Refresh Repository'),
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'res_model': 'pop.auth',
+                    'res_id': wiz_ob.id,
+                    'view_id': False,
+                    'target': 'new',
+                    'views': False,
+                    'type': 'ir.actions.act_window',
+                }
+
+    def create_folders(self):
+        """This function is to create folder inside folder inside root folder into root directory."""
 
         ticket = self.env['alfresco.operations'].search([], limit=1)
         ticket.get_auth_token_header()
@@ -203,31 +283,44 @@ class SaleOrderInherit(models.Model):
 
         base_url = ticket.alf_base_url + 'alfresco/api/-default-/public/alfresco/versions/1/nodes/-root-/children'
 
+        datas = {
+            "name": "",
+            "nodeType": "cm:folder",
+            "relativePath": "/Odoo/Sales Order/",
+        }
+
         headers = {
+            'Content-Type': 'application/json',
             'Authorization': 'Basic' + " " + ticket.alf_encoded_ticket
         }
 
-        for file in self.alf_file:
-            file_data = base64.b64decode(file.datas)
-
-            files = {
-                'filedata': file_data,
-                'name': (None, file.display_name),
-                'nodeType': (None, 'cm:content'),
-                'relativePath': (None, self.relative_path + str(self.name)),
-            }
-
-            response = requests.post(base_url, headers=headers, files=files)
-        if response.status_code == 201:
+        if not datas['name']:
+            datas.update({'name': 'Odoo', 'relativePath': ''})
+        response = requests.post(base_url, data=json.dumps(datas), headers=headers)
+        if response.status_code == 201 or response.status_code == 409:
+            if datas['name'] == 'Odoo':
+                datas.update({'name': 'Sales Order', 'relativePath': '/Odoo'})
+                response_1 = requests.post(base_url, data=json.dumps(datas), headers=headers)
+                if response_1.status_code == 201 or response_1.status_code == 409:
+                    if datas['name'] == 'Sales Order':
+                        datas.update({'name': str(self.name), 'relativePath': '/Odoo/Purchase Order'})
+                        response_2 = requests.post(base_url, data=json.dumps(datas), headers=headers)
+                        if response_2.status_code == 201:
+                            data_response_2 = json.loads(response_2.text)
+                            self.env['folder.details'].create({'name': data_response_2['entry']['name'],
+                                                               'folder_id': data_response_2['entry']['id']})
+            if self.is_active is False:
+                self.update({'is_active': True})
 
             # This Wizard is use to display the information which we are getting in Response.
 
-            wiz_ob = self.env['file.msg'].create({'pop_up': 'Your file has been uploaded.'})
+            wiz_ob = self.env['pop.folder'].create(
+                {'pop_up': "Folder Created"})
             return {
-                'name': _('Alert'),
+                'name': _('Create Folder'),
                 'view_type': 'form',
                 'view_mode': 'form',
-                'res_model': 'file.msg',
+                'res_model': 'pop.folder',
                 'res_id': wiz_ob.id,
                 'view_id': False,
                 'target': 'new',
@@ -235,12 +328,12 @@ class SaleOrderInherit(models.Model):
                 'type': 'ir.actions.act_window',
             }
         else:
-            wiz_ob = self.env['file.msg'].create({'pop_up': 'Please check your request and try again!'})
+            wiz_ob = self.env['pop.folder'].create({'pop_up': "Please check your request and try again!"})
             return {
-                'name': _('Alert'),
+                'name': _('Create Folder'),
                 'view_type': 'form',
                 'view_mode': 'form',
-                'res_model': 'file.msg',
+                'res_model': 'pop.folder',
                 'res_id': wiz_ob.id,
                 'view_id': False,
                 'target': 'new',
@@ -248,87 +341,27 @@ class SaleOrderInherit(models.Model):
                 'type': 'ir.actions.act_window',
             }
 
+# class InvoiceInherit(models.Model):
+#     _inherit = 'account.invoice'
 
-# class PurchaseOrderInherit(models.Model):
-#     _inherit = 'purchase.order'
-#
-#     notebook_ids = fields.One2many('alf.ui.functionality', 'id', string="")
-#     search_folder = fields.Many2one('folder.details', string="Folder")
-#
-#     def save_document_content(self):
-#
-#         ticket = self.env['alfresco.operations'].search([], limit=1)
-#         ticket.get_auth_token_header()
-#
-#         folder = self.env['folder.details'].search([('name', '=', self.sale_order_id.name)])
-#
-#         get_file_url = str(ticket.alf_base_url) + 'alfresco/api/-default-/public/alfresco/versions/1/nodes/' + str(
-#             folder.folder_id) + '/children'
-#
-#         headers = {
-#             'Content-Type': 'application/json',
-#             'Authorization': 'Basic' " " + str(ticket.alf_encoded_ticket)
-#         }
-#
-#         response = requests.get(get_file_url, headers=headers)
-#         if response.status_code == 200:
-#             response_data = json.loads(response.text)
-#
-#             result = self._cr.execute('delete from alf_ui_functionality')
-#
-#             if response_data['list']['pagination']['count'] >= 1:
-#                 for record in response_data['list']['entries']:
-#                     if record['entry']['name']:
-#                         name = record['entry']['name']
-#                         doc_id = record['entry']['id']
-#                         document_vals = [(0, 0, {
-#                             'id': self.id,
-#                             'document_name': name,
-#                             'document_id': doc_id
-#                         })]
-#                         existing = self.notebook_ids.search([('document_id', '=', doc_id),
-#                                                              ('document_name', '=', name)])
-#                         if existing:
-#                             existing.write({
-#                                 'document_name': name,
-#                                 'document_id': doc_id
-#                             })
-#                         else:
-#                             self.update({'notebook_ids': document_vals})
-#                             self._cr.commit()
-#                 wiz_ob = self.env['pop.auth'].create(
-#                     {'pop_up': 'Files Exported Successfully.'})
-#                 return {
-#                     'name': _('Refresh Repository'),
-#                     'view_type': 'form',
-#                     'view_mode': 'form',
-#                     'res_model': 'pop.auth',
-#                     'res_id': wiz_ob.id,
-#                     'view_id': False,
-#                     'target': 'new',
-#                     'views': False,
-#                     'type': 'ir.actions.act_window',
-#                 }
-#             else:
-#                 wiz_ob = self.env['pop.auth'].create(
-#                     {'pop_up': 'Folder does not contains any files.'})
-#                 return {
-#                     'name': _('Refresh Repository'),
-#                     'view_type': 'form',
-#                     'view_mode': 'form',
-#                     'res_model': 'pop.auth',
-#                     'res_id': wiz_ob.id,
-#                     'view_id': False,
-#                     'target': 'new',
-#                     'views': False,
-#                     'type': 'ir.actions.act_window',
-#                 }
+# class LotInherit(models.Model):
+#     _inherit = 'stock.production.lot'
+
+# class ContactsInherit(models.Model):
+#     _inherit = 'res.partner'
+
+# class MaintenanceInherit(models.Model):
+#     _inherit = 'maintenance.request'
+
+# class EquipmentInherit(models.Model):
+#     _inherit = 'maintenance.equipment'
 
 
 class AlfrescoUIFunctionality(models.Model):
     _name = 'alf.ui.functionality'
 
     sale_order_id = fields.Many2one('sale.order', string="ID")
+    purchase_id = fields.Many2one('purchase.order', string="ID")
     document_name = fields.Char("Doc Name")
     document_id = fields.Char("Doc ID")
 
@@ -337,6 +370,7 @@ class AlfrescoUIFunctionality(models.Model):
         ticket.get_auth_token_header()
 
         call = self.env['sale.order'].search([], limit=1)
+        call = self.env['purchase.order'].search([], limit=1)
 
         headers = {
             'Content-Type': 'application/json',
